67db630db5f23d5fd32d73ea62339fa104465b01
Thien <ledinhthienpm@gmail.com>
2025-09-16 17:12:03 +0700

    9/16/2025

---
+++ DSA/src/Array/CheckSorted.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class CheckSorted {
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 30, 40, 50 };
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				System.out.println("False");
+				return;
+			}
+		}
+		System.out.println("True");
+	}
+}

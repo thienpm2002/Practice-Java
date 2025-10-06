f17a967deb84429d3d8b3bca6b899ca9ef1f18f6
Thien <ledinhthienpm@gmail.com>
2025-10-06 11:36:15 +0700

    Find k largest element

---
+++ DSA/src/SearchingAlgorithms/FindKLargestElements.java
@@ -0,0 +1,31 @@
+package SearchingAlgorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindKLargestElements {
+	static List<Integer> solution(int[] arr, int k) {
+		int n = arr.length;
+		List<Integer> res = new ArrayList<>();
+		Arrays.sort(arr);
+		res.add(arr[n - 1]);
+		for (int i = n - 2; i >= 0; i--) {
+			if (arr[i] != arr[i + 1] && k - 1 > 0) {
+				res.add(arr[i]);
+				k--;
+			}
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 23, 12, 9, 30, 2, 50 };
+		int k = 3;
+		List<Integer> res = solution(arr, k);
+		for (int x : res) {
+			System.out.print(x + " ");
+		}
+
+	}
+}

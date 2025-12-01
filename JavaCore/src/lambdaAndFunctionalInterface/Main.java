b936b01b86404f96166ff403c725a8b90daae2ce
Thien <ledinhthienpm@gmail.com>
2025-12-01 17:00:49 +0700

    Functional interface StringMapper

--- JavaCore/src/lambdaAndFunctionalInterface/Main.java
+++ JavaCore/src/lambdaAndFunctionalInterface/Main.java
@@ -2,15 +2,12 @@ package lambdaAndFunctionalInterface;
 
 public class Main {
 	public static void main(String[] args) {
-		Calculator add = (a, b) -> a + b;
-		Calculator minus = (a, b) -> a - b;
-		Calculator multiplication = (a, b) -> a + b;
-		Calculator division = (a, b) -> a / b;
 
-		System.out.println("5 + 2 = : " + add.operate(5, 2));
-		System.out.println("5 - 2 = : " + minus.operate(5, 2));
-		System.out.println("5 * 2 = : " + multiplication.operate(5, 2));
-		System.out.println("5 / 2 = : " + division.operate(5, 2));
+		StringMapper mapper = (str) -> {
+			return str.replaceAll(" ", "").toUpperCase();
+		};
+
+		System.out.println("Le Dinh Thien qua mapper: " + mapper.uppercaseAndNotSpace("Le Dinh Thien"));
 
 	}
 }

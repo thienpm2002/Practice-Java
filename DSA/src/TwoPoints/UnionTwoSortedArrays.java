780195ba0df78d588c48c11344bff8f915486dc0
Thien <ledinhthienpm@gmail.com>
2025-12-03 16:19:46 +0700

    Union Two Sorted Arrays

--- DSA/src/TwoPoints/UnionTwoSortedArrays.java
+++ DSA/src/TwoPoints/UnionTwoSortedArrays.java
@@ -1,29 +1,58 @@
 package TwoPoints;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.ArrayList;
 
 public class UnionTwoSortedArrays {
 
+	public static void addUnique(ArrayList<Integer> res, int x) {
+		if (res.isEmpty() || res.get(res.size() - 1) != x) {
+			res.add(x);
+		}
+	}
+
+	static ArrayList<Integer> solution(int[] a, int[] b) {
+		int n = a.length;
+		int m = b.length;
+
+		ArrayList<Integer> res = new ArrayList<>();
+
+		int i = 0, j = 0;
+		while (i < n || j < m) {
+			if (i == n) {
+				addUnique(res, b[j]);
+				j++;
+				continue;
+			}
+			if (j == m) {
+				addUnique(res, a[i]);
+				i++;
+				continue;
+			}
+
+			if (a[i] < b[j]) {
+				addUnique(res, a[i]);
+				i++;
+			} else if (a[i] > b[j]) {
+				addUnique(res, b[j]);
+				j++;
+			} else {
+				addUnique(res, a[i]);
+				i++;
+				j++;
+			}
+		}
+
+		return res;
+
+	}
+
 	public static void main(String[] args) {
-		int[] a = { 1, 2, 3, 4, 5 };
-		int[] b = { 1, 2, 3, 6, 7 };
+		int[] a = { 1, 1, 1, 1, 1 };
+		int[] b = { 2, 2, 2, 2, 2 };
 
-		Set<Integer> s = new LinkedHashSet<>();
-		for (int x : a) {
-			s.add(x);
-		}
-		for (int x : b) {
-			s.add(x);
+		ArrayList<Integer> res = solution(a, b);
 
-		}
-
-		Integer[] arr = s.toArray(new Integer[0]);
-		Arrays.sort(arr);
-		for (int x : arr) {
-			System.out.print(x + " ");
-		}
+		res.forEach(System.out::println);
 
 	}
 }

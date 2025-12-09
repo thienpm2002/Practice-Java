f025158936dc1e0ecf677fba1b6b7a47e8a43fc5
Thien <ledinhthienpm@gmail.com>
2025-12-09 15:46:44 +0700

    Java I/O Bai2 2

--- JavaCore/src/JavaIO/Bai2.java
+++ JavaCore/src/JavaIO/Bai2.java
@@ -1,5 +1,6 @@
 package JavaIO;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -8,35 +9,24 @@ import java.util.Map;
 public class Bai2 {
 	public static void main(String[] args) {
 
-		StringBuilder sb = new StringBuilder();
-		Map<String, Integer> hm = new HashMap<>();
+		try (BufferedReader br = new BufferedReader(new FileReader("Bai2.txt"))) {
+			Map<String, Integer> hm = new HashMap<>();
+			String line;
 
-		try (FileReader fr = new FileReader("Bai2.txt")) {
-			int c;
-			while ((c = fr.read()) != -1) {
-				char ch = (char) c;
-				if (ch == ',' || ch == '.' || ch == ';') {
-					continue;
-				}
-				if (ch == ' ' || ch == '\n' || ch == '\r') {
-					if (sb.length() > 0) {
-						String word = sb.toString().toLowerCase();
-						hm.put(word, hm.getOrDefault(word, 0) + 1);
-						sb.setLength(0);
+			while ((line = br.readLine()) != null) {
+				line = line.replaceAll("[,.;]", " ");
+
+				String[] words = line.split("\\s+");
+
+				for (String w : words) {
+					if (!w.isEmpty()) {
+						w = w.toLowerCase();
+						hm.put(w, hm.getOrDefault(w, 0) + 1);
 					}
-				} else {
-					sb.append(ch);
 				}
 			}
 
-			if (sb.length() > 0) {
-				String word = sb.toString().toLowerCase();
-				hm.put(word, hm.getOrDefault(word, 0) + 1);
-			}
-
-			for (String key : hm.keySet()) {
-				System.out.println(key + ": " + hm.get(key));
-			}
+			hm.forEach((k, v) -> System.out.println(k + ": " + v));
 
 		} catch (IOException e) {
 			System.out.println("An error occurred while reading the file: " + e.getMessage());
